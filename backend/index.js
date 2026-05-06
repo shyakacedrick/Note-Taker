83108d3d599fa05d3e9d2204e60a2d4c11b3a040
shyakacedrick <student@sirafrica.org>
2026-05-06 11:51:46 +0300

    unistalled cors

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,10 @@
 console.log('====================| New Line! |=========================')
 
 const express = require('express')
-const cors = require('cors')
 const path = require('path')
 
 const app = express()
 
-app.use(cors())
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'dist')))
 
